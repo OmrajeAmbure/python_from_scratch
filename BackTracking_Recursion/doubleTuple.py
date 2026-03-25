4eb32df128cd6fe4498ba0a3a5fae87b7b682c7c
OmrajeAmbure <omrajeambure27@gmail.com>
2026-03-25 23:36:31 +0530

    Today Code Added

--- BackTracking_Recursion/doubleTuple.py
+++ BackTracking_Recursion/doubleTuple.py
@@ -1,6 +1,24 @@
-def doubleTup(numbers):
-    t = ();
-    for i in range(len(numbers)):
-        t = numbers*2;
-    return t;
-print(doubleTup([54,43,1,3,2]));
+num1=10
+num2=5 
+num3=0 
+num4=2 
+num5=10 
+
+print((num1==num5) and ((num2-num4*num3) == (num2-num3)));
+print((num2-num4*num3) <= ((num2-num4)*num3))
+
+# print(not(num3>=num4) and (num5/num2 == num4))
+print(not(num5>num4) or (num4+num2)>num1)
+print((num1==num5) and (not(num5/num2 == num1/num2)))
+
+num1 = 10
+num2 = 5
+num2 *= num1
+print(num2)
+
+num1 = 50
+num2 = 2
+num3 = 3
+num4 = 8
+result = num1/num4-num3*num2+num4
+print(result)
---
+++ OOPS/Demo.py
@@ -0,0 +1,9 @@
+class Demo:
+    def __init__(self):
+        pass
+    def config(self):
+        print("i5,56GB,");
+
+obj = Demo();
+Demo.config(obj);
+
